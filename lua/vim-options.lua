7e18214095c927d3ca315f01af9ae14473aa3973
Alix <alixfmcgill@gmail.com>
2024-09-22 02:28:01 -0500

    Vim options no text wrapping

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.cmd("set nowrap")
+vim.cmd("set formatoptions-=t")
 vim.g.mapleader = " "
 
 vim.wo.number = true
